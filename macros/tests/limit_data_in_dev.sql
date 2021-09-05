6e3091738f47e2d68d07cf1e7214501b1b57d196
Ohad Srur <ohadsrur@gmail.com>
2021-09-05 13:16:19 +1000

    limit data in dev macro

---
+++ macros/tests/limit_data_in_dev.sql
@@ -0,0 +1,6 @@
+{% limit_data_in_dev(column_name, dev_days_of_data=3, where_clause='where') %}
+{# where_clause options are ['where','and']#}
+{%- if target.name == 'dev' -%}
+{{where_clause}} {{column_name}} >= dateadd('day', - {{dev_days_of_data}}, {{ dbt_utils.current_timestamp() }})
+{%- endif -%}
+{% endmacro %}
