219f07b344341b9d582522870e7b23e965f399b4
Ohad Srur <ohadsrur@gmail.com>
2021-09-22 16:04:40 +1000

    If number change to cast to varchar

--- macros/utils/case_when_is_null_or_blank.sql
+++ macros/utils/case_when_is_null_or_blank.sql
@@ -1,5 +1,5 @@
 {%- macro case_when_is_null_or_blank(column_name,new_value) %}
-        CASE WHEN {{ column_name }} IS NULL OR {{ column_name }}=''
+        CASE WHEN {{ column_name }} IS NULL OR {{ column_name }}::varchar=''
         THEN '{{ new_value }}'::VARCHAR
         ELSE {{ column_name }}::VARCHAR
         END
