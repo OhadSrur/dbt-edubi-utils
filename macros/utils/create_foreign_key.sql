4ebb0cf1da1645e0e934424b3749074bb9b2b5b2
Ohad Srur <ohadsrur@gmail.com>
2021-10-04 09:10:03 +1100

    Adding logs to PK and FK macros

--- macros/utils/create_foreign_key.sql
+++ macros/utils/create_foreign_key.sql
@@ -1,3 +1,6 @@
 {% macro create_foreign_key(model_name, model_column_name, ref_table, ref_column_name) %}
   ALTER TABLE {{ model_name }} ADD FOREIGN KEY ({{model_column_name}}) REFERENCES {{ ref_table }}({{ref_column_name}})
+  {% if execute %}
+    {{ log('FK created for: ' ~model_name ~ ', column: ' ~ model_column_name ~ ', ref Table: ' ~ ref_table ~ ', ref column: '~ ref_column_name) }}
+  {% endif %}
 {% endmacro %}
--- macros/utils/create_primary_key.sql
+++ macros/utils/create_primary_key.sql
@@ -1,3 +1,6 @@
 {% macro create_primary_key(model_name, column_name) %}
   ALTER TABLE {{model_name}} ADD PRIMARY KEY ({{column_name}});
+  {% if execute %}
+    {{ log('PK created for: ' ~model_name ~ ', column: ' ~ column_name) }}
+  {% endif %}
 {% endmacro %}
