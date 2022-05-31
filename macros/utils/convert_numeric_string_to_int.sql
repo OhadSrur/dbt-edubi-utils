60f234183f689190f646f70852eacb18744d0874
Ohad Srur <osrur@edubi.com.au>
2022-05-31 13:20:19 +1000

    convert numeric to string macro

---
+++ macros/utils/convert_numeric_string_to_int.sql
@@ -0,0 +1,6 @@
+{% macro convert_numeric_string_to_int(string_value,default_none_int_value) %}
+        case when {{string_value}} ~ '^[0-9\.]+$' 
+        then ({{string_value}}::numeric % 10000000)::INT
+        else {{ not_int_value | default('null') }}
+        end 
+{%- endmacro %}
