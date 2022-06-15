b01bb3bd28d03e63f5386d90ef3f085a9d36aa2d
Ohad Srur <osrur@edubi.com.au>
2022-06-15 10:00:52 +1000

    Create macro: Case when is value then set to expression

---
+++ macros/utils/case_when_is_value_then_expression.sql
@@ -0,0 +1,6 @@
+{% macro case_when_is_value_then_expression(column_name,predicate,new_expression,else_expression) %}
+        CASE {{ column_name }} WHEN {{ predicate }}  
+        THEN {{ new_expression }} 
+        ELSE {{ else_expression }} 
+        END
+{%- endmacro %}
