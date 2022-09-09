7eb99afe7430b4e87955ed0af9d6801a58261187
Ohad Srur <osrur@edubi.com.au>
2022-09-09 15:53:13 +1000

    Create Alert template

---
+++ templates/alert_template.sql
@@ -0,0 +1,42 @@
+{{
+    config(
+        enabled=var('alert_',true)
+    )
+}}
+
+{{
+    edubi_utils.cte([('source','')])
+}},
+
+prep as (
+
+    select
+        -- Key
+
+        -- Ref Key
+        
+        -- Attributes
+        
+        -- Measures
+        
+        -- Flags
+
+    from source
+),
+
+final as (
+
+    select *,
+        
+        -- Meta
+        {{ edubi_utils.record_hash('prep') }}           as record_hash,
+        {{ current_timestamp() }} at time zone 'AEST'   as emitted_date_at,
+        {{ edubi_utils.convert_date_part_to_name(
+            'yyyy-mm-dd',
+            "current_timestamp at time zone 'AEST'") 
+        }}                                              as emitted_date
+
+    from prep
+)
+
+select * from final
