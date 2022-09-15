2de4a9af06578be162ddc1b0e861c623ee612bc0
Ohad Srur <osrur@edubi.com.au>
2022-09-16 06:09:30 +1000

    Add a var option for obfuscating description. default not to obfuscate

--- macros/utils/obfuscate_descriptions.sql
+++ macros/utils/obfuscate_descriptions.sql
@@ -1,11 +1,12 @@
-{# This macro will swap the value with the data requested from fake #}
+{# This macro will swap the keyword values with the key lookup value provided. Default is NOT to obfuscate description #}
+{# Add the following to obfuscate description. --vars 'obfuscate_description: true' #}
 {% macro obfuscate_descriptions(col_name,client_code,obfuscate_table='descriptive_codes',obfuscate_schema_name='fake') %}
-  {% if target.name == 'prod' %}
-            {{col_name}}
+  {% if target.name == 'prod' or not(var('obfuscate_description',false)) %}
+        {{col_name}}
   {%- else -%}     
-            coalesce((select replace({{col_name}},uf.keyword,uf.keyword_replacement)
-            FROM {{obfuscate_schema_name}}.{{obfuscate_table}} as uf
-            WHERE client_code=lower('{{ client_code }}') and {{col_name}} ilike '%' || uf.keyword || '%'
-            Limit 1),{{col_name}})
+        coalesce((select replace({{col_name}},uf.keyword,uf.keyword_replacement)
+        FROM {{obfuscate_schema_name}}.{{obfuscate_table}} as uf
+        WHERE client_code=lower('{{ client_code }}') and {{col_name}} ilike '%' || uf.keyword || '%'
+        Limit 1),{{col_name}})
   {%- endif -%}
 {% endmacro %}
