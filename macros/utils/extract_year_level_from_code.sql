3055106819f13687e0fb87de264252be29ebe3d5
Ohad Srur <osrur@edubi.com.au>
2022-09-20 16:37:40 +1000

    Create macro to extract year level number from class code

---
+++ macros/utils/extract_year_level_from_code.sql
@@ -0,0 +1,8 @@
+{# Use this macro to extract number from class codes #}
+{% macro extract_year_level_from_code(string_col,position='left',default_none_int_value='null') %}
+        case when {{ position }}({{string_col}},1) = 'K' then 0
+        when {{ position }}({{string_col}},2) ~ '^[0-9\.]+$' then {{ position }}({{string_col}},2)::int
+        when {{ position }}({{string_col}},1) ~ '^[0-9\.]+$' then {{ position }}({{string_col}},1)::int
+        else {{ default_none_int_value | default('null') }}
+        end
+{%- endmacro %}
