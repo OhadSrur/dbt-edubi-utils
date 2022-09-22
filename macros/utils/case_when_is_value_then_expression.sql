9ef8649c02b1081ae690a6b49b7167fa907fe407
Ohad Srur <osrur@edubi.com.au>
2022-09-22 12:10:57 +1000

    Add a default option to Else

--- macros/utils/case_when_is_value_then_expression.sql
+++ macros/utils/case_when_is_value_then_expression.sql
@@ -1,6 +1,6 @@
 {% macro case_when_is_value_then_expression(column_name,predicate,new_expression,else_expression) %}
         CASE {{ column_name }} WHEN {{ predicate }}  
         THEN {{ new_expression }} 
-        ELSE {{ else_expression }} 
+        ELSE {{ else_expression | default(column_name) }} 
         END
 {%- endmacro %}
