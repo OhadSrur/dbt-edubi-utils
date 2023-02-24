fab83088a6a4d12c43cf655e641c18f2c2027673
Ohad Srur <osrur@edubi.com.au>
2023-02-24 14:14:13 +1100

    Update macro to include class codes as 8.1

--- macros/utils/convert_string_to_int.sql
+++ macros/utils/convert_string_to_int.sql
@@ -1,6 +1,8 @@
 {% macro convert_string_to_int(string_value,default_none_int_value) %}
-        case when {{string_value}} ~ '^[0-9\.]+$' 
+        case when {{string_value}} ~ '^[0-9\.]+$' and {{string_value}} like '__.%'
         then {{string_value}}::INT
+        when {{string_value}} ~ '^[0-9\.]+$' and {{string_value}} like '_.%'
+        then left({{string_value}},1)::INT
         else {{ default_none_int_value | default('null') }}
         end 
 {%- endmacro %}
