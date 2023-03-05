c9e839714f76fbdf934327ce4e21e9ca92252df8
Ohad Srur <osrur@edubi.com.au>
2023-03-06 06:41:48 +1100

    Revert macro changes

--- macros/utils/convert_string_to_int.sql
+++ macros/utils/convert_string_to_int.sql
@@ -1,8 +1,6 @@
 {% macro convert_string_to_int(string_value,default_none_int_value) %}
-        case when {{string_value}} ~ '^[0-9\.]+$' and {{string_value}} like '__.%'
+        case when {{string_value}} ~ '^[0-9\.]+$' 
         then {{string_value}}::INT
-        when {{string_value}} ~ '^[0-9\.]+$' and {{string_value}} like '_.%'
-        then left({{string_value}},1)::INT
         else {{ default_none_int_value | default('null') }}
         end 
 {%- endmacro %}
