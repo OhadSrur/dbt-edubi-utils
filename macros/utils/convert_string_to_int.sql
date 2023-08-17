caf13cf3dec267fe8b17b43f5514a613fbf7df5b
Ohad Srur <osrur@edubi.com.au>
2023-08-18 08:30:51 +1000

    Update the macro to handle values provided with multiple dots, and allow to change the casting type

--- macros/utils/convert_string_to_int.sql
+++ macros/utils/convert_string_to_int.sql
@@ -1,6 +1,6 @@
-{% macro convert_string_to_int(string_value,default_none_int_value) %}
-        case when {{string_value}} ~ '^[0-9\.]+$' 
-        then {{string_value}}::INT
+{% macro convert_string_to_int(string_value,default_none_int_value,cast_type='int') %}
+        case when {{string_value}} ~ '^[0-9\.]+$' and {{string_value}} !~ '\.\.'
+        then {{string_value}}::{{cast_type}}
         else {{ default_none_int_value | default('null') }}
         end 
 {%- endmacro %}
