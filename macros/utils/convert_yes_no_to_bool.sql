993864bede67cbfe50753e843d8fb1119e62347c
Ohad Srur <osrur@edubi.com.au>
2023-09-11 17:51:13 +1000

    Add 'Y' 'N' options

--- macros/utils/convert_yes_no_to_bool.sql
+++ macros/utils/convert_yes_no_to_bool.sql
@@ -1,6 +1,6 @@
 {% macro convert_yes_no_to_bool(predicate) %}
-        CASE WHEN UPPER({{ predicate }}) = 'YES' THEN TRUE
-        WHEN UPPER({{ predicate }}) = 'NO' THEN FALSE
+        CASE WHEN UPPER({{ predicate }})in ('YES','Y') THEN TRUE
+        WHEN UPPER({{ predicate }}) in ('NO','N') THEN FALSE
         ELSE null
         END
 {%- endmacro %}
