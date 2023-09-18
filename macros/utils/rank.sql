91de7f53f07fa1b8828917fe92a610e3ddb47a22
Ohad Srur <osrur@edubi.com.au>
2023-09-19 07:58:54 +1000

    Add function_column_name parameter This will allow to pass specific arguements to the function

--- macros/utils/rank.sql
+++ macros/utils/rank.sql
@@ -1,6 +1,6 @@
-{% macro rank(partition_by,order_by,function_name='row_number') %}
-        {{function_name}}() 
+{% macro rank(partition_by,order_by,function_name='row_number',function_column_name='') %}
+        {{function_name}}({{function_column_name}}) 
         over(
             partition by {{partition_by}} 
-            order by {{order_by}})    
+            order by {{order_by}})
 {% endmacro %}
