55b1c3ae0305823b995e1ca34e7f0fff7b1e2e4f
Ohad Srur <osrur@edubi.com.au>
2024-03-14 09:01:28 +1100

    Update current_date macro to use now() function instead of current_date because it is providing unexpected results

--- macros/utils/current_date.sql
+++ macros/utils/current_date.sql
@@ -1,4 +1,4 @@
 {# return the current date #}
 {% macro current_date() %}
-        (current_date AT Time Zone '{{ var('client_timezone') }}')::date
+        (now() AT Time Zone '{{ var('client_timezone') }}')::date
 {% endmacro %}
