41a50302c68ebc8c64d9113bbb33181ad3f570cd
Ohad Srur <osrur@edubi.com.au>
2025-01-17 17:34:57 +1100

    Add a new pattern (number 2) for extracting subject_code

--- macros/utils/extract_subject_code.sql
+++ macros/utils/extract_subject_code.sql
@@ -1,4 +1,10 @@
 {# Extract subject_code by removing the year level from the assessment code #}
-{% macro extract_subject_code(string_col) %}
+{% macro extract_subject_code(string_col,pattern_number=1) %}
+        {% if pattern_number == 1 %}
+        {# example: 10CH -> CH #}
         SUBSTRING({{string_col}} FROM '\d*(\D.*)')
+        {% elif pattern_number == 2 %}
+        {# example: 07CH -> CH #}
+        right({{string_col}},2)
+        {% endif %}
 {%- endmacro %}
