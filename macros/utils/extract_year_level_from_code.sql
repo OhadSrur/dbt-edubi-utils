c4d2b37c7b49105542d41310c8edd6e8b4060046
Ohad Srur <osrur@edubi.com.au>
2025-05-01 12:29:44 +1000

    Improve the year level extraction logic to include validation for values between 0 and 13

--- macros/utils/extract_year_level_from_code.sql
+++ macros/utils/extract_year_level_from_code.sql
@@ -1,7 +1,7 @@
 {# Use this macro to extract number from class codes #}
 {% macro extract_year_level_from_code(string_col,position='left',default_none_int_value='null') %}
         case when {{ position }}({{string_col}},1) = 'K' then 0
-        when {{ position }}({{string_col}},2) ~ '^[0-9]+$' then {{ position }}({{string_col}},2)::int
+        when {{ position }}({{string_col}},2) ~ '^[0-9]+$'  and {{ position }}({{string_col}},2)::int between 0 and 13 then {{ position }}({{string_col}},2)::int
         when {{ position }}({{string_col}},1) ~ '^[0-9]+$' then {{ position }}({{string_col}},1)::int
         when {{ position }}({{string_col}},2) ~ '^Y[0-9\.]+$' then right(left({{string_col}},2),1)::int
         else {{ default_none_int_value | default('null') }}
