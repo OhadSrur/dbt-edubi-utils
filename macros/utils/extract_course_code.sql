c79aba7cfb27adb35be2fdf9560506d7ec23ab5b
Ohad Srur <osrur@edubi.com.au>
2025-05-27 16:59:59 +1000

    Enhance extract_course_code macro to allow customizable character for splitting class codes

--- macros/utils/extract_course_code.sql
+++ macros/utils/extract_course_code.sql
@@ -1,8 +1,8 @@
 {# Extract course_code by removing the class number from the end of the class code #}
-{% macro extract_course_code(string_col,pattern_number=1) %}
+{% macro extract_course_code(string_col,pattern_number=1,char_to_split='.') %}
         {% if pattern_number == 1 %}
         {# example: 10CH.1 -> 10CH #}
-                SPLIT_PART({{string_col}},'.',1)
+                SPLIT_PART({{string_col}},'{{char_to_split}}',1)
         {% elif pattern_number == 2 %}
         {# example: 10CH1 -> 10CH #}
                 SUBSTRING({{string_col}} FROM '^(\d+[A-Z]+)')
