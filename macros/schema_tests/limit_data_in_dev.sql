0b7b80897ed7054f713b6e4effa5a0adc57ed57b
Ohad Srur <osrur@edubi.com.au>
2025-07-05 07:58:36 +1000

    Fix the deprecation warnings in the limit_data_in_dev.sql macro

--- macros/schema_tests/limit_data_in_dev.sql
+++ macros/schema_tests/limit_data_in_dev.sql
@@ -1,6 +1,11 @@
-{% limit_data_in_dev(column_name, dev_days_of_data=3, where_clause='where') %}
-{# where_clause options are ['where','and']#}
-{%- if target.name == 'dev' -%}
-{{where_clause}} {{column_name}} >= dateadd('day', - {{dev_days_of_data}}, {{ dbt_utils.current_timestamp() }})
-{%- endif -%}
+{% macro limit_data_in_dev(column_name, dev_days_of_data=3, where_clause='where') %}
+  {# where_clause can be 'where' or 'and' #}
+
+  {% if target.name == 'dev' %}
+    {{ where_clause }} {{ column_name }} >= dateadd(
+        'day',
+        -{{ dev_days_of_data }},
+        {{ dbt_utils.current_timestamp() }}
+    )
+  {% endif %}
 {% endmacro %}
