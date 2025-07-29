e2f1e17460c5b01db6ad2101975ef0ddd7c1776c
Ohad Srur <osrur@edubi.com.au>
2025-07-29 14:17:27 +1000

    Update macro extract_year_level_from_code Nullify the year_level if greater than 13

--- macros/utils/extract_year_level_from_code.sql
+++ macros/utils/extract_year_level_from_code.sql
@@ -2,6 +2,7 @@
 {% macro extract_year_level_from_code(string_col,position='left',default_none_int_value='null') %}
         case when {{ position }}({{string_col}},1) = 'K' then 0
         when {{ position }}({{string_col}},2) ~ '^[0-9]+$'  and {{ position }}({{string_col}},2)::int between 0 and 13 then {{ position }}({{string_col}},2)::int
+        when {{ position }}({{string_col}},2) ~ '^[0-9]+$'  and {{ position }}({{string_col}},2)::int > 13 then null
         when {{ position }}({{string_col}},1) ~ '^[0-9]+$' then {{ position }}({{string_col}},1)::int
         when {{ position }}({{string_col}},2) ~ '^Y[0-9\.]+$' then right(left({{string_col}},2),1)::int
         else {{ default_none_int_value | default('null') }}
