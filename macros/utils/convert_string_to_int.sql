5a03738a506614b15440a8449e555f635199761f
Ohad Srur <osrur@edubi.com.au>
2025-08-11 08:13:09 +1000

    Amend the regex to exclude values with extra dot in convert_string_to_int

--- macros/utils/convert_string_to_int.sql
+++ macros/utils/convert_string_to_int.sql
@@ -1,5 +1,5 @@
 {% macro convert_string_to_int(string_value,default_none_int_value,cast_type='int') %}
-        case when {{string_value}} ~ '^[0-9\.]+$' and {{string_value}} !~ '\.\.'
+        case when {{string_value}} ~ '^[0-9]+(\.[0-9]+)?$' and {{string_value}} !~ '\.\.'
         then {{string_value}}::{{cast_type}}
         else {{ default_none_int_value | default('null') }}
         end 
